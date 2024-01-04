22699205fbeb8577406dee4f7eddda61256f4f7c
Sujeet <sujeet4010@gmail.com>
2024-01-04 14:09:15 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Sujeet',
+    author_email= 'sujeet4010@gamil.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
